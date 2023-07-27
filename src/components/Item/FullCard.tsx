a312ef21d78a9b830306dafb5606c278b8d12df6
kevinmarangoni <kevin-marangoni_@hotmail.com>
2023-07-26 23:59:18 -0300

    starts item component

--- src/components/Item/FullCard.tsx
+++ src/components/Item/FullCard.tsx
@@ -1,16 +1,95 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import IKanji from '@/types/kanji'
+import styled from 'styled-components'
 
 interface IItemProps {
     item: IKanji
 }
 
 const FullCard:React.FC<IItemProps> = ({item}) => {
+  const [isBack, setIsBack] = useState<boolean>(false);
+
   return (
-    <div>FullCard</div>
-  )
+    <Container onClick={() => {setIsBack(!isBack)}}>
+      <Content>
+        <Side>
+          {!isBack ? (
+            <>
+              <CharacterContainer>
+                <h2>hiragana</h2>
+                <p>{item.hiragana}</p>
+              </CharacterContainer>
+              <CharacterContainer>
+                <h2>katakana</h2>
+                <p>{item.katakana}</p>
+              </CharacterContainer>
+            </>
+          ) : (
+            <RomajiContainer>
+              <h2>romaji</h2>
+              <p>{item.romaji}</p>
+            </RomajiContainer>
+          )}
+        </Side>
+      </Content>
+    </Container>
+  );
 }
 
 export default FullCard
+
+const Container = styled.div`
+  cursor: pointer;
+  border-radius: 5px;
+  border: 1px solid green;
+  display: flex;
+  min-width: 141px;
+  justify-content: center;
+  align-items: center;
+`
+
+const Content = styled.div`
+  margin: 5px;
+`
+
+const Side = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  justify-content: center;
+  align-items: space-between;
+  height: 85px;
+`
+
+const CharacterContainer = styled.div`
+  *{
+    text-align: center;
+  }
+  h2{
+    font-weight: 400;
+    font-size: 14px;
+  }
+  p{
+    font-weight: 600;
+    font-size: 40px;
+    line-height: 65px;
+  }
+`
+
+const RomajiContainer = styled.div`
+  *{
+    text-align: center;
+  }
+  h2{
+    font-weight: 400;
+    font-size: 14px;
+  }
+  p{
+    font-weight: 600;
+    font-size: 40px;
+    line-height: 65px;
+  }
+  width: 130px;
+`
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,44 +1,58 @@
-'use client'
+"use client";
 
-import React,{useEffect, useState} from 'react'
-import IKanji from '@/types/kanji'
-import ApiMethods from '@utils/api'
+import React, { useEffect, useState } from "react";
+import IKanji from "@/types/kanji";
+import ApiMethods from "@utils/api";
+import FullCard from "@/components/Item/FullCard";
+import styled from "styled-components";
 
+const Home: React.FC = () => {
+  const [hasFetch, setHasFetch] = useState<boolean>(false);
+  const [data, setData] = useState<Array<IKanji>>([]);
 
-const Home:React.FC = () => {
-  const [hasFetch, setHasFetch] = useState<boolean>(false)
-  const [data, setData] = useState<Array<IKanji>>([])
-
-  const fetchData = async () =>{
-    const response = await ApiMethods.getTyped(['Seion'])
-    if(response?.status === 200){
-      setData(response?.data)
-      setHasFetch(true)
-      return
+  const fetchData = async () => {
+    //const response = await ApiMethods.getTyped(["Seion"]);
+    const response = await ApiMethods.getRandom();
+    if (response?.status === 200) {
+      setData(response?.data);
+      setHasFetch(true);
+      return;
     }
-  }
+  };
 
-  useEffect(()=>{
-    fetchData()
-  },[])
-  
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
-      {(data.length > 0) ? 
-        <>
-          {data.map((item: IKanji)=>{
-            return (
-              <p key={item.id}>{item.hiragana} e {item.katakana} = {item.romaji}</p>
-            )
-          })}
-        </>
-      :
-        <>
-          Carregando
-        </>
-      }
+      <Container>
+        <Content>
+          {data.length > 0 ? (
+            <>
+              {data.map((item: IKanji) => {
+                return <FullCard item={item} key={item.id} />;
+              })}
+            </>
+          ) : (
+            <>Carregando</>
+          )}
+        </Content>
+      </Container>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
+
+const Container = styled.section`
+  padding: 10px;
+`;
+
+const Content = styled.section`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  justify-content: center;
+  align-items: center;
+`;
